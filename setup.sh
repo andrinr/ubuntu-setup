f6c129edafa809311efd162d9f8b745c0eb2a1b0
andrinr <andrinrehmann@gmail.com>
2022-12-25 11:13:52 +0100

    add cat of pubkey

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 mkdir projects
-ssh-keygen
 git config --global user.name "andrinr"
 git config --global user.email "andrinrehmann@gmail.com"
 sudo apt-get install python3
 wget https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 chmod 777 Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 sudo ./Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
+
+ss-keygen
+cat  ~/.ssh/id_rsa.pub
