78884c389c2971667b23fa7abc1bdbec01f90940
andrinr <andrinrehmann@gmail.com>
2022-12-26 10:39:02 +0100

    remove sudo

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git config --global user.email "andrinrehmann@gmail.com"
 sudo apt-get install python3
 wget https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 chmod 777 Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
-sudo ./Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
+./Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 
 ssh-keygen
 cat  ~/.ssh/id_rsa.pub
