e859d810e65a0669f35e2ed33825045ab1916eb4
andrinr <andrinrehmann@gmail.com>
2022-12-26 10:26:19 +0100

    fix typos

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-mkdir projects
+mkdir ~/projects
 git config --global user.name "andrinr"
 git config --global user.email "andrinrehmann@gmail.com"
 sudo apt-get install python3
@@ -6,5 +6,5 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64
 chmod 777 Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 sudo ./Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
 
-ss-keygen
+ssh-keygen
 cat  ~/.ssh/id_rsa.pub
