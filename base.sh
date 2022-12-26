b614d06cb5aac747b73dad19cee6bc2a06bbb408
andrinr <andrinrehmann@gmail.com>
2022-12-26 11:42:01 +0100

    split scripts

---
+++ base.sh
@@ -0,0 +1,6 @@
+mkdir ~/projects
+git config --global user.name "andrinr"
+git config --global user.email "andrinrehmann@gmail.com"
+
+ssh-keygen
+cat  ~/.ssh/id_rsa.pub
---
+++ python.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python3
+wget https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
+chmod 777 Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
+bash ./Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
